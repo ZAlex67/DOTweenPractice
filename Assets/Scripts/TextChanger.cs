14f16e1b4e058dea4a70be458e82f6b54d5fc947
Aleksandr <zabrodinalex67@gmail.com>
2024-05-07 21:11:51 +0900

    Fix text

--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 public class TextChanger : MonoBehaviour
 {
     [SerializeField] private Text _text1;
-    [SerializeField] private Text _text2;
-    [SerializeField] private Text _text3;
     [SerializeField] private float _duration;
     [SerializeField] private int _repeats;
     [SerializeField] private LoopType _loopType;
 
     private void Start()
     {
-        _text1.DOText("Я заменил этот текст", _duration).SetLoops(_repeats, _loopType);
-        _text2.DOText(". Это дополнение к тексту", _duration).SetLoops(_repeats, _loopType);
-        _text3.DOText("Я взломал этот текст", _duration, true, ScrambleMode.All).SetLoops(_repeats, _loopType);
+        Sequence sequence = DOTween.Sequence().SetLoops(_repeats, _loopType);
+
+        sequence.Append(_text1.DOText("Я заменил этот текст", _duration));
+        sequence.Append(_text1.DOText(". Это дополнение к тексту", _duration).SetRelative());
+        sequence.Append(_text1.DOText("Я взломал этот текст", _duration, true, ScrambleMode.All));
     }
 }
